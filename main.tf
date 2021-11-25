a4ea0bed7b0a8ca5666a005e013391684999d507
Mario Robles <mario.robles@digitalonus.com>
2021-11-24 20:18:18 -0600

    adding a ssh key to terraform

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ data "azurerm_resource_group" "main" {
   name = "mario-robles"
 }
 
+
 resource "azurerm_kubernetes_cluster" "main" {
   name                = "boutiqueaks"
   location            = data.azurerm_resource_group.main.location
@@ -31,62 +32,126 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "vault-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+
+
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "centralus"
+    resource_group_name = "mario-robles"
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = "mario-robles"
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefixes       = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "vault-nic"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "centralus"
+    resource_group_name          = "mario-robles"
+    allocation_method            = "Dynamic"
 
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "vaultvm"
-  location              = data.azurerm_resource_group.main.location
-  resource_group_name   = data.azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "centralus"
+    resource_group_name = "mario-robles"
 
-  delete_os_disk_on_termination = true
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "VAULT"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "*"
+        destination_port_range     = "8200"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
+}
+
+# Create network interface
+resource "azurerm_network_interface" "myterraformnic" {
+    name                      = "mynic"
+    location                  = "centralus"
+    resource_group_name       = "mario-robles"
+
+    ip_configuration {
+        name                          = "mynicConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+    }
+
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+    network_interface_id      = azurerm_network_interface.myterraformnic.id
+    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+resource "azurerm_ssh_public_key" "example" {
+  name                = "myssh"
+  resource_group_name = "mario-robles"
+  location            = "centralus"
+  public_key          = file("~/.ssh/id_rsa.pub")
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+    name                  = "myVM"
+    location              = "centralus"
+    resource_group_name   = "mario-robles"
+    size                  = "Standard_B1s"
+    network_interface_ids = [
+      azurerm_network_interface.myterraformnic.id,
+      ]
+    
+
+    os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+      admin_ssh_key {
+    username   = "myvault"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "boutique"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
+
+    computer_name  = "myvm"
+    admin_username = "myvault"
+    admin_password = "Vault1234!"
     disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
+
+ 
 }
